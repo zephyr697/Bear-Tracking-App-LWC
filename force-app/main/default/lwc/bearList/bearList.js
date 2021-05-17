e0353b05aaf99c7466694713ed5d7449ae3bbacb
Sachin Maurya <maurya.sachin2@gmail.com>
2021-05-17 22:39:54 +0530

    unit 4,5

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,14 +1,23 @@
+import { publish, MessageContext } from 'lightning/messageService';
+import BEAR_LIST_UPDATE_MESSAGE from '@salesforce/messageChannel/BearListUpdate__c';
+import { NavigationMixin } from 'lightning/navigation';
 import { LightningElement, wire } from 'lwc';
-import ursusResources from '@salesforce/resourceUrl/ursus_park';
 /** BearController.searchBears(searchTerm) Apex method */
 import searchBears from '@salesforce/apex/BearController.searchBears';
-export default class BearList extends LightningElement {
+export default class BearList extends NavigationMixin(LightningElement) {
 	searchTerm = '';
-	@wire(searchBears, {searchTerm: '$searchTerm'})
-	bears;
-	appResources = {
-		bearSilhouette: `${ursusResources}/img/standing-bear-silhouette.png`,
-	};
+  bears;
+  @wire(MessageContext) messageContext;
+  @wire(searchBears, {searchTerm: '$searchTerm'})
+  loadBears(result) {
+    this.bears = result;
+    if (result.data) {
+      const message = {
+        bears: result.data
+      };
+      publish(this.messageContext, BEAR_LIST_UPDATE_MESSAGE, message);
+    }
+  }
 	handleSearchTermChange(event) {
 		// Debouncing this method: do not update the reactive property as
 		// long as this function is being called within a delay of 300 ms.
@@ -22,5 +31,18 @@ export default class BearList extends LightningElement {
 	}
 	get hasResults() {
 		return (this.bears.data.length > 0);
+	}
+	handleBearView(event) {
+		// Get bear record id from bearview event
+		const bearId = event.detail;
+		// Navigate to bear record page
+		this[NavigationMixin.Navigate]({
+			type: 'standard__recordPage',
+			attributes: {
+				recordId: bearId,
+				objectApiName: 'Bear__c',
+				actionName: 'view',
+			},
+		});
 	}
 }
